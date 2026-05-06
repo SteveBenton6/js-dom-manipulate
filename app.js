4a36bb7532cfcc37815255a61f2d6836d17b7b29
Steve Benton <steve@bentonemail.uk>
2026-05-06 13:12:28 +0100

    add innerText

---
+++ app.js
@@ -0,0 +1,56 @@
+// Example 1 -  Working with innerText & textContent
+console.log("\nSECTION 1 - Working with innerText & textContent");
+
+// Access h1 text content
+const h1IT = document.querySelector("h1");
+console.log(h1IT.innerText);
+
+// Access whole body text content
+const bodyqS = document.body.innerText;
+console.log(bodyqS);
+
+// Update h1 text content (in future linked to events so not an HTML change)
+h1IT.innerText = "Hello JavaScipt Chapter 14 Updated DOM Manipulate World!";
+
+// Example 2 -  innerHTML
+console.log("\nSECTION 2 - innerHTML");
+// No Code
+
+// Example 3 -  value, src, href, and more
+console.log("\nSECTION 3 - value, src, href, and more");
+// No Code
+
+// Example 4 -  Getting & Setting Attributes
+console.log("\nSECTION 4 - Getting & Setting Attributes");
+// No Code
+
+// Example 5 -  Finding Parent/Children/Siblings
+console.log("\nSECTION 5 - Finding Parent/Children/Siblings");
+// No Code
+
+// Example 6 -  Changing Multiple Elements
+console.log("\nSECTION 6 - hanging Multiple Elements");
+
+// Example 7 -  Altering Styles
+console.log("\nSECTION 7 - Altering Styles");
+
+// Example 8 -  getComputedStyle
+console.log("\nSECTION 8 - getComputedStyle");
+
+// Example 9 -  Manipulating Classes
+console.log("\nSECTION 9 - Manipulating Classes");
+
+// Example 10 -  Creating Elements
+console.log("\nSECTION 10 - Creating Elements");
+
+// Example 11 -  Append, Prepend, & insertBefore
+console.log("\nSECTION 11 - Append, Prepend, & insertBefore");
+
+// Example 12 -  removeChild & remove
+console.log("\nSECTION 12 - removeChild & remove");
+
+// Example 13 -  NBA Scores Chart Pt1.
+console.log("\nSECTION 13 - NBA Scores Chart Pt1.");
+
+// Example 14 -  NBA Scores Chart Refactor
+console.log("\nSECTION 14 - NBA Scores Chart Refactor");
